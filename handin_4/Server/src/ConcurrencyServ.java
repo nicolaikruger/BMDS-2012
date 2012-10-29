f5d4d1d3f04a2c998ea188554e107b0b37c35878
Sigurt Bladt Dinesen <sigurt91@gmail.com>
2012-10-29 17:38:12 +0100

    new file:   ../Server/resources/task-manager-revised.xml 	new file:   ../Server/src/Calendar.java 	modified:   ../Server/src/ConcurrencyServ.java 	new file:   ../Server/src/Task.java 	new file:   ../Server/src/TaskList.java 	new file:   ../lib/Connection.java 	new file:   ../lib/Marshall.java 	deleted:    ../ConcurrencyServ.java 	deleted:    ../Connection.java

--- handin_4/Server/src/ConcurrencyServ.java
+++ handin_4/Server/src/ConcurrencyServ.java
@@ -1,21 +1,37 @@
 import java.util.concurrent.*;
 import java.util.Random;
+import java.net.DatagramPacket;
+import java.io.*;
 
 public class ConcurrencyServ {
 	BlockingQueue<Integer> q = new LinkedBlockingQueue<>(100);
+	Connection con = new Connection();
 	Random rnd = new Random();
 
 	public ConcurrencyServ(){
 		new WorkerThread(q).start();
-		while(true) {
-			try {
-				synchronized(q) {
-					q.put(rnd.nextInt());
-				}
-			}catch (InterruptedException e){
-				e.printStackTrace();
-			}
+	//	while(true) {
+	//		try {
+	//			synchronized(q) {
+	//				q.put(rnd.nextInt());
+	//			}
+	//		}catch (InterruptedException e){
+	//			e.printStackTrace();
+	//		}
+	//	}
+		listen();
+	}
+
+	private void listen(){
+		DatagramPacket d = con.serverReceiveFromClient();
+		String syn = null;
+		try {
+			syn = (String) new ObjectInputStream(new ByteArrayInputStream(d.getData())).readObject();
+		} catch (IOException | ClassNotFoundException e ) {
+			e.printStackTrace();
 		}
+		System.out.println(syn);
+		con.serverSendToClient("Hi, i'm a server. Who are you", d);
 	}
 
 	public static void main(String[] args) {
